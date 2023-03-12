f44dbedbf24a9a4d299876e35e143e34d22489c8
ad <jerzyana@gmail.com>
2023-03-12 17:55:41 +0100

    zadanie dzień 4

---
+++ ConsoleApp-day4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.Design;
+
+var imie = "Ewa";
+var kobieta = true;
+var wiek = 33;
+var wiek2 = 26;
+var wiek3 = 17;
+
+
+if (kobieta = true)
+    {
+    if (wiek2 < 30)
+        {
+        Console.WriteLine("kobieta, ponizej 30 lat");
+        }
+    };    
+
+if (imie == "Ewa" && wiek == 33)
+    {
+    Console.WriteLine("Ewa, lat " + wiek);
+    };
+
+if (wiek3 < 18)
+    {
+    Console.WriteLine("Niepełnoletni męszczyzna");
+    }
+else if (wiek >= 18)
+    {
+    Console.WriteLine("Pełnoletni męszczyzna");
+    };
+
+
+
+    
+

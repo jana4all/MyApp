7879ddc904da22352127a7b516aa610894f48dde
ad <jerzyana@gmail.com>
2023-03-12 21:20:04 +0100

    Zadanie dzień 5

--- MyApp1/Program.cs
+++ MyApp1/Program.cs
@@ -1,2 +1,70 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! jestem tu ");
+﻿int number = 4566;
+int counter0 = 0;
+int counter1 = 0;
+int counter2 = 0;
+int counter3 = 0;
+int counter4 = 0;
+int counter5 = 0;
+int counter6 = 0;
+int counter7 = 0;
+int counter8 = 0;
+int counter9 = 0;
+
+string numberInString = number.ToString();
+char[] letters = numberInString.ToArray();
+
+foreach (var letter in letters)
+{
+    if (letter == '0')
+    {
+        counter0++;
+    };
+    if (letter == '1')
+    {
+        counter1++;
+    };
+    if (letter == '2')
+    {
+        counter2++;
+    };
+    if (letter == '3')
+    {
+        counter3++;
+    };
+    if (letter == '4')
+    {
+        counter4++;
+    };
+    if (letter == '5')
+    {
+        counter5++;
+    };
+    if (letter == '6')
+    {
+        counter6++;
+    };
+    if (letter == '7')
+    {
+        counter7++;
+    };
+    if (letter == '8')
+    {
+        counter8++;
+    };
+    if (letter == '9')
+    {
+        counter9++;
+    };
+};
+
+Console.WriteLine("Liczba zadana = " + number);
+Console.WriteLine("ilość liczy 0 = " + counter0);
+Console.WriteLine("ilość liczy 1 = " + counter1);
+Console.WriteLine("ilość liczy 2 = " + counter2);
+Console.WriteLine("ilość liczy 3 = " + counter3);
+Console.WriteLine("ilość liczy 4 = " + counter4);
+Console.WriteLine("ilość liczy 5 = " + counter5);
+Console.WriteLine("ilość liczy 6 = " + counter6);
+Console.WriteLine("ilość liczy 7 = " + counter7);
+Console.WriteLine("ilość liczy 8 = " + counter8);
+Console.WriteLine("ilość liczy 9 = " + counter9);
